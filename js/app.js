4b478156fa6ff79e7c1f7fd323c129f0fc18c68b
Jay Critze <jaycritze@Jays-Air.home>
2020-07-06 12:18:42 -0400

    new version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,2 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".u-scroll-disappear").css({ "opacity": "0" })
-    } else {
-      $(".u-scroll-disappear").css({ "opacity": "1" })
-    }
-  })
-})
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".u-scroll-container-background").css({ "opacity": "1" })
-    } else {
-      $(".u-scroll-container-background").css({ "opacity": "0" })
-    }
-  })
-})
+
